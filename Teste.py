403e1bef64c2125247c6b2b74f3603c03c7d2dac
murilomike <murilomike@outlook.com>
2025-06-26 18:36:51 -0300

    commit

--- Teste.py
+++ Teste.py
@@ -1,12 +1,3 @@
 projeto = "Bem vindo ao Projeto Lighter Tracker DB"
 print(projeto)
 
-def function(parametro)
-    while projeto != "Sair":
-        print("Digite 'Sair' para encerrar o programa.")
-        parametro = input("Digite um comando: ")
-        if parametro == "Sair":
-            print("Encerrando o programa...")
-            break
-        else:
-            print(f"Você digitou: {parametro}")
